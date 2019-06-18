45ea9310ee99821d91b768bf5ccadafa07ad72ef
王海龙 <wanghailongbj@163.com>
2019-06-18 18:04:55 +0800

    add KVO Category for NSObject

---
+++ KVOOverride/KVOOverride/NSObject+KVO.h
@@ -0,0 +1,17 @@
+//
+//  NSObject+KVO.h
+//  KVOOverride
+//
+//  Created by 王海龙 on 2019/3/15.
+//  Copyright © 2019 WHL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (KVO)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KVOOverride/KVOOverride/NSObject+KVO.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+KVO.m
+//  KVOOverride
+//
+//  Created by 王海龙 on 2019/3/15.
+//  Copyright © 2019 WHL. All rights reserved.
+//
+
+#import "NSObject+KVO.h"
+
+@implementation NSObject (KVO)
+
+@end
